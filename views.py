12edbe4705b7bf0cc05c9a293b7e597acd05d392
Harika-982 <113515816+Harika-982@users.noreply.github.com>
2023-06-28 22:17:27 +0530

    Add files via upload

--- views.py
+++ views.py
@@ -1,48 +1,81 @@
 from django.shortcuts import render
-from django.http import  HttpResponse
-from Doctor.models import doctorModel,doctorreportmodel
-from forensic.models import *
+from django.http import HttpResponse
+from forensic.forms import *
+from Doctor.models import *
+import speech_recognition as sr
+import pyttsx3
 
-def adminlogin(request):
-    return render(request, "admin/adminlogin.html")
+def frnsclogin(request):
+    return render(request, "frnsc/frnsclogin.html")
 
-def adminloginentered(request):
+
+
+def frnscloginentered(request):
     if request.method == 'POST':
         uname=request.POST['uname']
         passwd=request.POST['upasswd']
-        if uname =='admin' and passwd =='admin':
-            return render(request,"admin/adminloginentered.html")
+        if uname =='frnsc' and passwd =='frnsc':
+            return render(request,"frnsc/frnscloginentered.html")
         else:
             return HttpResponse("invalied credentials")
-    return render(request, "admin/adminloginentered.html")
+    return render(request, "frnsc/frnscloginentered.html")
 
 
-def doctordetails(request):
-    qs=doctorModel.objects.all()
-    return render(request,'admin/doctordetails.html',{"object":qs})
-
-def logout(request):
-    return render(request,"index.html")
-
-def activatedoctor(request):
-    if request.method == 'GET':
-        uname = request.GET.get('pid')
-        print(uname)
-        status = 'Activated'
-        print("pid=", uname, "status=", status)
-        doctorModel.objects.filter(id=uname).update(status=status)
-        qs = doctorModel.objects.all()
-        return render(request,"admin/doctordetails.html", {"object": qs})
 
 
-def finalreport(request):
-    qs=doctorreportmodel.objects.all()
-    return render(request,'admin/finalreport.html',{"object":qs})
+def bodydetails(request):
+        if request.method == 'POST':
+            form =bodydetailsForm(request.POST, request.FILES)
+            if form.is_valid():
+                form.save()
+                return render(request, 'frnsc/bodydetails.html')
+        else:
+            form =bodydetailsForm()
+        return render(request, 'frnsc/bodydetails.html', {'form': form})
 
 
-def adminbodydetails(request):
-    qs = bodydetails.objects.all()
-    return render(request, 'admin/adminbodydetails.html',{"object": qs})
+def forensicreport(request):
+    return render(request, 'frnsc/frnscreportid.html')
+
+def frnscreport1(request):
+    if request.method == 'POST':
+        bid=request.POST.get('t1')
+        qs=doctorreportmodel.objects.filter(bid=bid)
+        for x in qs:
+            pmi=x.pmi
+        r = sr.Recognizer()
+        print("r:", r)
+
+        # Function to convert text to
+        # speech
+        def SpeakText(command):
+
+            # Initialize the engine
+            engine = pyttsx3.init()
+            print("engine:", engine)
+            engine.say(command)
+            print("hello:", engine.say(command))
+            engine.runAndWait()
+            print(engine.runAndWait())
+
+        # Loop infinitely for user to
+        # speak
+
+        SpeakText(pmi)
+        return render(request,'frnsc/finalreport1.html',{"object":qs})
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
